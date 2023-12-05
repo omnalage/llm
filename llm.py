5912cdc67570e2ffc74f74ef756ce893884ab457
yashk6615 <145190332+yashk6615@users.noreply.github.com>
2023-12-05 11:25:17 +0530

    removing comments

--- llm.py
+++ llm.py
@@ -13,24 +13,24 @@ import torch
 import numpy as np
 # # how dataset was generated
 
-# # load imdb data
-# imdb_dataset = load_dataset("imdb")
+# load imdb data
+imdb_dataset = load_dataset("imdb")
 
-# # define subsample size
-# N = 1000 
-# # generate indexes for random subsample
-# rand_idx = np.random.randint(24999, size=N) 
+# define subsample size
+N = 1000 
+# generate indexes for random subsample
+rand_idx = np.random.randint(24999, size=N) 
 
-# # extract train and test data
-# x_train = imdb_dataset['train'][rand_idx]['text']
-# y_train = imdb_dataset['train'][rand_idx]['label']
+# extract train and test data
+x_train = imdb_dataset['train'][rand_idx]['text']
+y_train = imdb_dataset['train'][rand_idx]['label']
 
-# x_test = imdb_dataset['test'][rand_idx]['text']
-# y_test = imdb_dataset['test'][rand_idx]['label']
+x_test = imdb_dataset['test'][rand_idx]['text']
+y_test = imdb_dataset['test'][rand_idx]['label']
 
-# # create new dataset
-# dataset = DatasetDict({'train':Dataset.from_dict({'label':y_train,'text':x_train}),
-#                              'validation':Dataset.from_dict({'label':y_test,'text':x_test})})
+# create new dataset
+dataset = DatasetDict({'train':Dataset.from_dict({'label':y_train,'text':x_train}),
+                              'validation':Dataset.from_dict({'label':y_test,'text':x_test})})
 # load dataset
 dataset = load_dataset('shawhin/imdb-truncated')
 dataset
